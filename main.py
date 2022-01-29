0a04fcaff13a84c9515e65091fd7d612930cf7e9
Shreyas Minocha <sm-git@riseup.net>
2022-01-28 22:11:17 -0600

    Remove demo bar graph

--- main.py
+++ main.py
@@ -6,28 +6,18 @@ import numpy as np
 st.title('This Blows')
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = 'https://www.ndbc.noaa.gov/data/realtime2/KIKT.txt'
 
 @st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+def load_data():
+    data = pd.read_csv(DATA_URL, delim_whitespace=True)
     return data
 
 data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+data = load_data()
+data_load_state.text('Done! (using st.cache)')
 
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
 
 fig = go.Figure(data=
     go.Scatterpolar(
