c9b6b0eb0cfe1dd2d8b7b5dd601c24b40ca5aa44
Michael Wong <mwongmaui@gmail.com>
2022-01-29 01:55:41 -0600

    Add altair graph

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import plotly.figure_factory as ff
 import numpy as np
+import altair_viewer
 
 STATIONS = ['KIKT', 'KBQX', 'KMIS']
 
@@ -46,3 +47,63 @@ fig = go.Figure(data=
 fig.update_layout(showlegend=False)
 
 st.plotly_chart(fig, use_container_width=True)
+
+#line chart
+
+np.random.seed(42)
+source = pd.DataFrame(np.cumsum(np.random.randn(100, 3), 0).round(2),
+                    columns=['KMIS', 'KAPT', 'KIKT'], index=pd.RangeIndex(100, name='Time'))
+source = source.reset_index().melt('Time', var_name='Station', value_name='Wind Direction')
+
+# Create a selection that chooses the nearest point & selects based on x-value
+nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                        fields=['Time'], empty='none')
+
+# The basic line
+line = alt.Chart(source).mark_line(interpolate='basis').encode(
+    x='Time:Q',
+    y='Wind Direction:Q',
+    color='Station:N'
+)
+
+# Transparent selectors across the chart. This is what tells us
+# the x-value of the cursor
+selectors = alt.Chart(source).mark_point().encode(
+    x='Time:Q',
+    opacity=alt.value(0),
+).add_selection(
+    nearest
+).properties(
+    title='Wind Direction for the Past 45 Days'
+)
+selectors.configure_title(
+    fontSize=40,
+    font='Courier',
+    anchor='start',
+    color='gray'
+)
+# Draw points on the line, and highlight based on selection
+points = line.mark_point().encode(
+    opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+)
+
+# Draw text labels near the points, and highlight based on selection
+text = line.mark_text(align='left', dx=5, dy=-5).encode(
+    text=alt.condition(nearest, 'Wind Direction:Q', alt.value(' '))
+)
+
+
+# Draw a rule at the location of the selection
+rules = alt.Chart(source).mark_rule(color='gray').encode(
+    x='Time:Q',
+).transform_filter(
+    nearest
+)
+
+# Put the five layers into a chart and bind the data
+base = alt.layer(
+    line, selectors, points, rules, text
+).properties(
+    width=600, height=300
+)
+base
